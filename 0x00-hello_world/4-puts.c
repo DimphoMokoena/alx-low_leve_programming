87500cae6fd2526361dcf0395bf683df7fe23964
Dimpho <dimphomokoena46@gmail.com>
2022-09-08 16:12:58 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+#include <string.h>
+
+/**
+* main -entry point
+* use the put function
+* program prints "Programming is like building a multilingual puzzle"
+* return (0)
+*/
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
